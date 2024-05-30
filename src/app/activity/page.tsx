7bfb279eaf3cd6fb17ffb3e6023c7fbf21828022
Piinxluna <pingpingkuss@gmail.com>
2024-05-30 20:30:10 +0700

    Initialize project

---
+++ src/app/activity/page.tsx
@@ -0,0 +1,7 @@
+export default function Activity() {
+  return (
+    <div>
+      <h1>Activity page</h1>
+    </div>
+  )
+}
---
+++ src/app/award/page.tsx
@@ -0,0 +1,7 @@
+export default function Award() {
+  return (
+    <div>
+      <h1>Award page</h1>
+    </div>
+  )
+}
