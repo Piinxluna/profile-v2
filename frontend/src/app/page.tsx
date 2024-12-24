3cc3f83a1e135206d0328d5fa1b2ce005b6f5993
Piinxluna <pingpingkuss@gmail.com>
2024-12-24 14:06:53 +0700

    Under construction

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,9 @@
 'use client'
 
 import { useEffect, useState } from 'react'
+import LinkComponent from 'next/link'
+import Image from 'next/image'
+import { Link } from 'lucide-react'
 
 export default function Home() {
   const [isThai, setIsThai] = useState(true)
@@ -10,11 +13,34 @@ export default function Home() {
   }, [isThai])
 
   return (
-    <div className='w-full h-full text-black dark:text-white flex justify-center items-center'>
+    <div className='w-full min-h-[36rem] text-black dark:text-white flex justify-center items-center space-x-6 space-y-0 flex-wrap'>
+      <Image
+        src='/UnderConstruction.png'
+        alt='Under construction'
+        width={500}
+        height={500}
+        objectFit='contain'
+        className='w-72 h-60 md:w-96 md:h-80'
+      />
       <div>
-        {isThai
-          ? 'เว็บไซต์นี้กำลังอยู่ในช่วงพัฒนา'
-          : 'This website is under construction!'}
+        <p className='text-2xl font-semibold'>
+          {isThai
+            ? 'เว็บไซต์นี้กำลังอยู่ในช่วงพัฒนา'
+            : 'This website is under construction!'}
+        </p>
+        <div className='flex space-x-3 my-2 text-lg'>
+          <p>
+            {isThai
+              ? 'สามารถดู profile เก่าได้ที่'
+              : 'You can view previous profile at'}
+          </p>
+          <div className='flex space-x-2 items-center hover:text-my-blue'>
+            <Link size={20} />
+            <LinkComponent href='https://piinxluna.github.io/profile/'>
+              piinxluna.github.io/profile/
+            </LinkComponent>
+          </div>
+        </div>
       </div>
     </div>
   )
