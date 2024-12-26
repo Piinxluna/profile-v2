fe95aced25277560be0941ef06d63332e361041b
Piinxluna <pingpingkuss@gmail.com>
2024-12-26 15:29:43 +0700

    Fix loading screen bug

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -2,7 +2,7 @@ export default function Loading({ isFullPage }: { isFullPage?: boolean }) {
   if (isFullPage) {
     return (
       // -- Solid background version --
-      <div className='w-screen h-screen bg-my-indigo-80 text-white text-xl font-bold fixed top-0 left-0 flex justify-center items-center space-x-4'>
+      <div className='w-screen h-screen bg-my-indigo-80 text-white text-xl font-bold fixed top-0 left-0 flex justify-center items-center space-x-4 z-50'>
         <div className='w-12 h-12 rounded-full animate-spin border-8 border-solid border-white border-t-transparent'></div>
         <p>Loading...</p>
       </div>
